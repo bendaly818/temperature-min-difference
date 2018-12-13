0b46b25dc53196b2fdc24d4608d83c4b6ef52d00
Ben Daly <bendaly0403@gmail.com>
2018-12-13 16:46:39 +1300

    Initial Implementation

---
+++ weather.py
@@ -0,0 +1,57 @@
+import re
+
+#finds all integers in a string and formats them using a regular expression returning a list of valid integers
+def get_integers(a):
+    return [int(float(s)) for s in re.findall(r'\d+(?:\.\d+)?', a)]
+
+#takes a list of integers and finds the minimum value(s) from the list along with their indexes
+def locate_min(a):
+    smallest = min(a)
+    return smallest, [index for index, element in enumerate(a) 
+                      if smallest == element]
+
+def get_minimum_temperature_difference(filepath):
+    with open(filepath, "r") as w:
+        weather_data = w.readlines()
+        weather_data = [i.rstrip().split() for i in weather_data]
+        
+    days = []
+    differences = []
+    
+    for item in weather_data:
+        #ignore any empty lists
+        if len(item) != 0:
+            #ignoring the column types and the month average row and making sure that there are valid 
+            if not item[0].isalpha() and len(get_integers(item[1])) != 0 and len(get_integers(item[2])) != 0:
+                day = item[0]
+                max_temp = get_integers(item[1])[0]
+                min_temp = get_integers(item[2])[0]
+                if max_temp < min_temp:
+                    print("There is a minimum temperature that was more than a maximum temperature on day {}, it was ignored.".format(day))
+                    continue
+                else:
+                    days.append(day)
+                    differences.append(max_temp-min_temp)
+    
+    minimum_difference = locate_min(differences)
+
+    days_with_minimum_difference = []
+
+    for index in minimum_difference[1]:
+        days_with_minimum_difference.append(days[index])
+        
+    if len(days_with_minimum_difference) > 1:
+        days = ", ".join(str(day) for day in days_with_minimum_difference)
+        print("The minimum difference in temperature for the month is " + str(minimum_difference[0]) + " degrees on the days " + days)
+    else:
+        day = days_with_minimum_difference[0]
+        print("The minimum difference in temperature for the month is " + str(minimum_difference[0]) + " degrees on day " + day)
+    
+
+
+
+
+
+
+
+
